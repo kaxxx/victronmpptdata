41c0d51326b25315ef415117c6838f723bbdb1de
kax <kay.berkemeier@googlemail.com>
2020-10-21 10:38:38 +0200

    new rest endpoint for singe date values some css some html

--- complete/src/main/java/com/example/uploadingfiles/FileUploadController.java
+++ complete/src/main/java/com/example/uploadingfiles/FileUploadController.java
@@ -3,9 +3,11 @@ package com.example.uploadingfiles;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.example.uploadingfiles.db.SolarDataRepository;
+import com.example.uploadingfiles.model.SolarData;
 import com.example.uploadingfiles.parse.CsvFileParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -13,12 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -45,6 +42,12 @@ public class FileUploadController {
 		this.storageService = storageService;
 	}
 
+	@GetMapping("/rest/{date}")
+	@ResponseBody
+	public Optional<SolarData> read(@PathVariable("date") int date) {
+		Optional<SolarData> foundDate = solarDataRepository.findById(date);
+		return foundDate;
+	}
 
 	@GetMapping("/")
 	public String listUploadedFiles(Model model) throws IOException {
